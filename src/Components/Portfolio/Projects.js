3bf45c9903ab7f63cbd996be62446b7ba1408da7
Piyumi Gunasekara <160240571+piyumigunasekara03@users.noreply.github.com>
2024-07-11 16:55:16 +0530

    updates

--- src/Components/Portfolio/Projects.js
+++ src/Components/Portfolio/Projects.js
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
-import './Projects.css'; // Import the CSS file for styling
-// import Project1 from './project1.png';
-// import Project2 from './project1.2.png';
-// import Project3 from '/project2.png';
-// import Project4 from '/project2.1.png';
+import './Projects.css'; 
+
+import Project1 from './project1.png';
+import Project1Hover from './project1.2.png';
+import Project2 from './project2.png';
+import Project2Hover from './project2.1.png';
+import Project3 from './project1.png';
+import Project3Hover from './project1.2.png';
+import Project4 from './project2.png';
+import Project4Hover from './project2.1.png';
+import Project5 from './project1.png';
+import Project5Hover from './project1.2.png';
+import Project6 from './project2.png';
+import Project6Hover from './project2.1.png';
 
 function Projects() {
   const [hoveredIndex, setHoveredIndex] = useState(null);
@@ -17,37 +26,18 @@ function Projects() {
   }; 
 
   const images = [
-    { defaultSrc: '/project1.png', hoverSrc: '/project1.2.png', name: 'Project1' },
-    { defaultSrc: '/project2.png', hoverSrc: '/project2.1.png', name: 'Project2' },
-    { defaultSrc: '/project1.png', hoverSrc: '/project1.2.png', name: 'Project3' }, 
-    { defaultSrc: '/project2.png', hoverSrc: '/project2.1.png', name: 'Project4' },
-    { defaultSrc: '/project1.png', hoverSrc: '/project1.2.png', name: 'Project5' },
-    { defaultSrc: '/project2.png', hoverSrc: '/project2.1.png', name: 'Project6' },
+    { defaultSrc: Project1, hoverSrc: Project1Hover, name: 'Project 1' },
+    { defaultSrc: Project2, hoverSrc: Project2Hover, name: 'Project 2' },
+    { defaultSrc: Project3, hoverSrc: Project3Hover, name: 'Project 3' }, 
+    { defaultSrc: Project4, hoverSrc: Project4Hover, name: 'Project 4' },
+    { defaultSrc: Project5, hoverSrc: Project5Hover, name: 'Project 5' },
+    { defaultSrc: Project6, hoverSrc: Project6Hover, name: 'Project 6' },
   ];
 
   return (
     <div className="portfolio-container">
       <h2 className="subtitle">Our Portfolio</h2>
       <div className="image-container">
-<<<<<<< HEAD
-        <img
-          src={hovered ? '/div.p-uvt64 4.png' : '/Project 2 - draft.png'}
-          alt="Hover-image"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          className="hover-image"
-        />
-        {hovered && <div className="image-name">Lorem Ipsum</div>}
-        <img
-          src={hovered ? '/download 2 1.png' : '/Project 1 - draft.png'}
-          alt="Hover-image"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          className="hover-image"
-        />
-        {hovered && <div className="image-name">Lorem Ipsum</div>}
-        
-=======
         {images.map((image, index) => (
           <div
             className="image-wrapper"
@@ -63,7 +53,6 @@ function Projects() {
             {hoveredIndex === index && <div className="image-name">{image.name}</div>}
           </div>
         ))}
->>>>>>> 6c471afe3be1303d2ea2f29f3b188efe15600029
       </div>
     </div>
   );
