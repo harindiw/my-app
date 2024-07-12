483b09a7953a614892634a358ea153845f5f1f61
harindi weerasinghe <harindiweerasinghe2001@gmail.com>
2024-07-12 15:46:10 +0530

    push12

--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -1,31 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Banner.css';
 import slide1 from './slide1.png';
-import slide2 from './slide2.png'; 
-import slide3 from './slide3.png'; 
+import slide2 from './slide2.png';
+import slide3 from './slide3.png';
+//import Button from '@mui/material/Button';
+//import MobileStepper from '@mui/material/MobileStepper';
+//import Paper from '@mui/material/Paper';
+//import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+//import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
+//import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 
 const Banner = () => {
+  const slides = [slide1, slide2, slide3];
+  const CollectionSize = slides.length;
+  const theme = useTheme();
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const slideInterval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+    }, 2000); // Change slide every 2 seconds
+
+    return () => clearInterval(slideInterval);
+  }, [slides.length]);
+
+  // Function to go to the next picture
+  const goToNextPicture = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length + 1);
+  };
+
   return (
     <div className="banner">
-      <nav className="navbar">
-        <div className="logo">AV IOT SOLUTIONS</div>
-        <ul className="nav-links">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About Us</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-
-      
-
-      <div className="slider">
-        <div className="slides">
-          <img src={slide1} alt="Slide 1" />
-          <img src={slide2} alt="Slide 2" />
-          <img src={slide3} alt="Slide 3" />
-        </div>
+     
+      <div className="slider-container">
+        {/*<Paper square elevation={0} className="paper">
+          <Typography>{`Slide ${currentSlide + 1}`}</Typography>
+        </Paper> */}
+        <img
+          src={slides[currentSlide]}
+          className="slider-image"
+          alt={`Slide ${currentSlide + 1}`}
+        />
+        {/*<MobileStepper
+          variant="text"
+          position="static"
+          steps={CollectionSize}
+          nextButton={
+            <Button
+              size="small"
+              onClick={goToNextPicture}
+              disabled={currentSlide === CollectionSize - 1}
+            >
+              Next
+              {theme.direction !== 'rtl' ? (
+                <KeyboardArrowRight />
+              ) : (
+                <KeyboardArrowLeft />
+              )}
+            </Button>
+          }
+        /> */}
       </div>
 
       <div className="banner-content">
@@ -33,8 +68,6 @@ const Banner = () => {
         <p>We have developed top-notch IOT devices and assisted numerous organizations in achieving economic success.</p>
         <button className="contact-btn">Contact Us</button>
       </div>
-      
-
     </div>
   );
 };
